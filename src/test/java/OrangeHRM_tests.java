a79a2cb47fdefa78a726ae74b4559836be38879a
Tanya Korlyukova <36532585+tanyakorlyukova@users.noreply.github.com>
2019-03-22 12:43:14 +0300

    commit

--- src/test/java/OrangeHRM_tests.java
+++ src/test/java/OrangeHRM_tests.java
@@ -19,39 +19,10 @@ public class OrangeHRM_tests extends BaseTest {
         loginPage.invalidLogin(username, password, message);
     }
 
-    @Test(groups = "login")
+    @Test
     public void LoginTest_Valid() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.validLogin("Admin", "admin123", "Welcome Admin");
     }
 
-    /*@Test(groups = "login")
-    public void LoginTest_Empty() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.invalidLogin("", "", "Username cannot be empty");
-    }
-
-    @Test(groups = "login")
-    public void LoginTest_InvalidUsername() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.invalidLogin("invalid", "admin123", "Invalid credentials");
-    }
-
-    @Test(groups = "login")
-    public void LoginTest_InvalidPassword() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.invalidLogin("Admin", "invalid", "Invalid credentials");
-    }
-
-    @Test(groups = "login")
-    public void LoginTest_Invalid() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.invalidLogin("invalid", "invalid", "Invalid credentials");
-    }
-
-    @Test(groups = "login")
-    public void LoginTest_SpecialSymbols() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.invalidLogin("@#$%&^~", "@#$%&^~", "Invalid credentials");
-    } */
 }
