5a6c5392126ab8152fa02000a6d56a927a2b5acd
Tanya Korlyukova <36532585+tanyakorlyukova@users.noreply.github.com>
2019-03-27 00:29:06 +0300

    commit

--- src/test/java/OrangeHRM_tests.java
+++ src/test/java/OrangeHRM_tests.java
@@ -1,9 +1,11 @@
 import org.testng.annotations.*;
+import pages.LoginPage;
+import pages.UsersPage;
 
 public class OrangeHRM_tests extends BaseTest {
 
-    @DataProvider(name = "logindata")
-    public Object[][] data() {
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
         return new String[][] {
                 {"", "", "Username cannot be empty"},
                 {"invalid", "admin123", "Invalid credentials"},
@@ -13,11 +15,11 @@ public class OrangeHRM_tests extends BaseTest {
         };
     }
 
-    @Test(dataProvider = "logindata")
+ /*   @Test(dataProvider = "loginData")
     public void LoginTest_Invalid(String username, String password, String message) {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.invalidLogin(username, password, message);
-    }
+    } */
 
     @Test
     public void LoginTest_Valid() {
@@ -25,4 +27,20 @@ public class OrangeHRM_tests extends BaseTest {
         loginPage.validLogin("Admin", "admin123", "Welcome Admin");
     }
 
+    @DataProvider(name = "addUserData")
+    public Object[][] addUserdata() {
+        return new String[][] {
+                {"valid", "Fiona Grace", "fiooonna", "password", "password"},
+                {"empty", "", "", "", ""},
+                {"invalid", "d", "d", "d", "d"},
+                {"invalid password confirmation", "Hannah Flores", "hanna", "password", "otherpassword"}
+        };
+    }
+
+    @Test(dataProvider = "addUserData")
+    public void addUserTest(String testTitle, String empName, String username, String password, String confPassword) {
+        UsersPage usersPage = new UsersPage(driver);
+        usersPage.openAddUserForm();
+        usersPage.addUser(testTitle, empName, username, password, confPassword);
+    }
 }
