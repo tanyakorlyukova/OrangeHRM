93be59c74be134ce088abc0dfa04713b132af04f
Tanya Korlyukova <36532585+tanyakorlyukova@users.noreply.github.com>
2019-04-04 00:37:18 +0300

    commit

---
+++ src/test/java/tests/TestDataProvider.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.testng.annotations.DataProvider;
+
+public class TestDataProvider {
+
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        return new String[][] {
+                {"", "", "Username cannot be empty"},
+                {"invalid", "admin123", "Invalid credentials"},
+                {"Admin", "invalid", "Invalid credentials"},
+                {"invalid", "invalid", "Invalid credentials"},
+                {"@#$%&^~", "@#$%&^~", "Invalid credentials"}
+        };
+    }
+
+    @DataProvider(name = "addValidUserData")
+    public Object[][] addValidUserdata() {
+        return new String[][] {
+                {"John Smith", "hserlarry", "password", "password"},  //all fields - valid
+                {"John Smith", "htislarry", "", ""},   //password is not required
+                {"John Smith", "dhhhl", "password", "password"}  //5 characters in Username (at least 5)
+        };
+    }
+
+    @DataProvider(name = "addInvalidUserData")
+    public Object[][] addInvalidUserdata() {
+        return new String[][] {
+                {"", "", "", ""},   //empty
+                {"d", "d", "d", "d"},   //invalid
+                {"John Smith", "hanna", "password", "otherpassword"},   //different password
+                {"John Smith", "Admin", "password", "password"},   //user Admin exist
+                {"John Smith", "page", "password", "password"},  //4 characters in Username (at least 5)
+                {"notexistemployee", "exist", "password", "password"},  //employee does not exist
+                {"John Smith", "laaaaaary", "sssssss", "sssssss"}   //7 characters in Password (at least 8)
+        };
+    }
+}
